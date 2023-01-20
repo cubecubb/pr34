9654d6719ac4b16e2080fdc1e16b2d0f1fe7ae44
cubecubb <99070706+cubecubb@users.noreply.github.com>
2023-01-21 01:17:39 +0700

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,86 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
+from kivy.lang import Builder
+from kivy.core.window import Window
+
+Window.size = (500,700)
+
+Builder.load_file('calc.kv')
+
+class MyLayout(Widget):
+	def clear(self):
+		self.ids.calc_input.text = '0'
+
+	def button_press(self, button):
+		prior = self.ids.calc_input.text
+		if "Error" in prior:
+			prior = ''
+
+		if prior == "0":
+				self.ids.calc_input.text = ''
+				self.ids.calc_input.text = f'{button}'
+		else: 
+			self.ids.calc_input.text = f'{prior}{button}'
+	
+
+	def remove(self):
+		prior = self.ids.calc_input.text
+		prior = prior[:-1]
+		self.ids.calc_input.text = prior
+	
+	def pos_neg(self):
+		prior = self.ids.calc_input.text
+		if "-" in prior:
+			 self.ids.calc_input.text = f'{prior.replace("-", "")}'
+		else:
+			self.ids.calc_input.text = f'-{prior}'
+
+	def dot(self):
+		prior = self.ids.calc_input.text
+		num_list = prior.split("+")
+		
+		if "+" in prior and "." not in num_list[-1]:
+			prior = f'{prior}.'
+			self.ids.calc_input.text = prior
+
+		elif "." in prior:
+			pass
+		else:
+			prior = f'{prior}.'
+			self.ids.calc_input.text = prior
+
+	def math_sign(self, sign):
+		prior = self.ids.calc_input.text
+		self.ids.calc_input.text = f'{prior}{sign}'
+
+	
+	def equals(self):
+		prior = self.ids.calc_input.text
+		try:
+			answer = eval(prior)
+			self.ids.calc_input.text = str(answer)
+		except:
+			self.ids.calc_input.text = "Error"
+
+		'''
+		# Addition
+		if "+" in prior:
+			num_list = prior.split("+")
+			answer = 0.0
+			# loop thru our list
+			for number in num_list:
+				answer = answer + float(number)
+
+			# print the answer in the text box
+			self.ids.calc_input.text = str(answer)
+		'''
+
+class CalculatorApp(App):
+	def build(self):
+		return MyLayout()
+
+if __name__ == '__main__':
+	CalculatorApp().run()
+
+
